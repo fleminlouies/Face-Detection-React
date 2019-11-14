b3da2ee68bfa80fdbbb312ee5f9ffa1c35ffb656
fleminlouies <56334936+fleminlouies@users.noreply.github.com>
2019-11-15 01:48:24 +0530

    Updates

---
+++ precache-manifest.e7929df76ddc5106976f75e7de259c50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57a1fe0789b8bf38badc31c277f9c86a",
+    "url": "/Face-Detection-React/index.html"
+  },
+  {
+    "revision": "7b08a73f0a3e57dd5174",
+    "url": "/Face-Detection-React/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "a0451d160b74e109f761",
+    "url": "/Face-Detection-React/static/css/main.b9579a2e.chunk.css"
+  },
+  {
+    "revision": "7b08a73f0a3e57dd5174",
+    "url": "/Face-Detection-React/static/js/2.68eaa812.chunk.js"
+  },
+  {
+    "revision": "a0451d160b74e109f761",
+    "url": "/Face-Detection-React/static/js/main.53b3bb1e.chunk.js"
+  },
+  {
+    "revision": "011c285c44ade6443932",
+    "url": "/Face-Detection-React/static/js/runtime-main.ac48c3ae.js"
+  }
+]);
